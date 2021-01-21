1bc6d6d08bd7092d076fa52637e914520dbfb004
alexkobylansky <alex.kobylansky@gmail.com>
2021-01-21 20:11:41 +0200

    successful project

---
+++ js/script.js
@@ -0,0 +1,57 @@
+window.addEventListener("DOMContentLoaded", function () {
+  function setCursorPosition(pos, elem) {
+    elem.focus();
+    if (elem.setSelectionRange) elem.setSelectionRange(pos, pos);
+    else if (elem.createTextRange) {
+      let range = elem.createTextRange();
+      range.collapse(true);
+      range.moveEnd("character", pos);
+      range.moveStart("character", pos);
+      range.select()
+    }
+  }
+
+  function mask(event) {
+    let matrix = "+38(___)___-__-__",
+      i = 0,
+      def = matrix.replace(/\D/g, ""),
+      val = this.value.replace(/\D/g, "");
+    if (def.length >= val.length) val = def;
+    this.value = matrix.replace(/./g, function (a) {
+      return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a
+    });
+    if (event.type === "blur") {
+      if (this.value.length === 2) this.value = ""
+    } else setCursorPosition(this.value.length, this)
+  }
+
+  let input = document.querySelector("#tel");
+  input.addEventListener("input", mask, false);
+  input.addEventListener("focus", mask, false);
+  input.addEventListener("blur", mask, false);
+
+// ОТправка формы
+  const form = document.querySelector('#form');
+  form.addEventListener('submit', formSend);
+
+  async function formSend(e) {
+    e.preventDefault();
+
+    let formData = new FormData(form);
+    form.classList.add('sending');
+
+    let response = await fetch('/mail.php', {
+      method: 'POST',
+      body: formData,
+    });
+    if (response.ok) {
+      let result = await response.json();
+      alert(result.message);
+      form.reset();
+      form.classList.remove('sending');
+    } else {
+      alert('Сталася помилка, повторіть, будь ласка, спробу');
+      form.classList.remove('sending');
+    }
+  }
+});
